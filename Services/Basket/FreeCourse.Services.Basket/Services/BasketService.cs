8fb5feedcd9410cb179bd421843dcc3bfefcc09a
Suleyman Aylar <suleyman_aylar@hotmail.com>
2023-10-29 18:25:33 +0300

    Basket MicroServis Completed

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        [HttpGet]
+        public async Task<Response<bool>> Delete(string userId)
+        {
+           var status= await _redisService.GetDb().KeyDeleteAsync(userId);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket  Not Found!", 404);
+        }
+        [HttpPost]
+        public async Task<Response<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+
+            if (String.IsNullOrEmpty(existBasket))
+                return Response<BasketDto>.Fail("Basket Not Found!", 404);
+
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+        }
+
+        [HttpDelete]
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Basket Could Not Update Or Save", 500);
+        }
+    }
+}
---
+++ Shared/FreeCourse.Shared/Services/ISharedIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    public interface ISharedIdentityService
+    {
+        public string GetUserId { get; }
+
+    }
+}
