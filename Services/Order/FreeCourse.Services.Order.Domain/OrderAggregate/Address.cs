b1bdd64764454a2a77b1c99f464f5a38b7013b34
Suleyman Aylar <suleyman_aylar@hotmail.com>
2023-11-04 02:54:11 +0300

    order microservice add,DDD architecture

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject
+    {
+        public string Provice { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+        public Address(string provice, string district, string street, string zipCode, string line)
+        {
+            Provice = provice;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Provice;
+            yield return District;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
+    }
+}
