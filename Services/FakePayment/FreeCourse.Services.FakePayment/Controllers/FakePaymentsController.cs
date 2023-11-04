9b17b8c6b2d5c0cabb667521d9f862a5a414444e
Suleyman Aylar <suleyman_aylar@hotmail.com>
2023-11-04 15:41:40 +0300

    fake payment microservice complete

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Shared.BaseController;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivedPayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
